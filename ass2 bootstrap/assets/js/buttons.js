8543a83fe59fded0fa4c19d0cdc10f673138a3f9
MariFromNZ <91533215+MariFromNZ@users.noreply.github.com>
2024-06-26 22:22:15 +1200

    search form&h1div structure&design +bootstrap
    Bg, header, logo - done
    Need to check typography in the form (colour, size for labels and inputs)

---
+++ ass2 bootstrap/assets/js/buttons.js
@@ -0,0 +1,6 @@
+// for elements in progress roundBtn
+// var addAddressBtn = document.getElementById('addAddressBtn');
+// addAddressBtn.addEventListener('click', function() {
+//     alert('Sorry, still in development');
+// });
+
---
+++ ass2 bootstrap/assets/js/index.js
@@ -0,0 +1,22 @@
+
+
+// for elements in progress registerBtn
+// var registerBtn = document.getElementById('registerBtn');
+// registerBtn.addEventListener('click', function() {
+//     alert('Sorry, still in development');
+// });
+
+// // for login
+// var loginBtn = document.getElementById('loginBtn');
+// loginBtn.addEventListener('click', function() {
+//     var userNameInput = document.getElementById('name').value;
+//     var passwordInput = document.getElementById('password').value;
+//     if(userNameInput === 'Mariia' && passwordInput === '123')
+//     {
+
+//     }
+//     else{
+//         alert('Username or password is not correct. Try username - Mariia, password - 123')
+//     }      
+// });
+
