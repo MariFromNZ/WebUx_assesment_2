71383d5635c48d4d88993012a14214e13dcea083
MariFromNZ <91533215+MariFromNZ@users.noreply.github.com>
2024-06-29 23:25:04 +1200

    Card is done, modal bg blur, validation started
    Cleaned files (as used in the previous assessment file)
    transport card is done
    bg finally fixed - blurred
    started validation for searachTransportForm

--- ass2 bootstrap/assets/js/main.js
+++ ass2 bootstrap/assets/js/main.js
@@ -1,1 +1,35 @@
 
+
+
+//Validation for searchTransportForm
+function validateForm() {
+
+    var errors = [];
+
+    if (document.getElementById("pickUpLocation").value === "Select pick up location") {
+        errors.push("Pick up location.");
+    }
+
+    if (document.getElementById("dropOffLocation").value === "Select drop-off location") {
+        errors.push("Drop-off location.");
+    }
+
+    if (errors.length > 0) {
+        errors.unshift("Please fill out all required fields to continue:");//Insert text before all errors 
+
+        alert(errors.join("\n"));//Separates errors
+        return false;  //Prevent form submission
+    }
+
+    document.getElementById("exampleModal").classList.remove("hidden");//Shows modal
+
+    return false;  //Prevent form submission to show modal first
+}
+
+//Hide searchTransportForm and shows availableTransportForm
+document.getElementById("modalOkButton").onclick = function () {
+    document.getElementById("exampleModal").classList.add("hidden");
+    document.getElementById("searchTransportForm").classList.add("hidden");
+    document.getElementById("h1Title").classList.add("hidden");
+    document.getElementById("availableTransport").classList.remove("hidden");
+};
