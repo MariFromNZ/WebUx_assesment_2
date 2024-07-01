c8d4bd65e3686f8cde918124008eb1ddca5e853c
MariFromNZ <91533215+MariFromNZ@users.noreply.github.com>
2024-07-01 18:28:37 +1200

    Finished confirmationForm validation

--- ass2 bootstrap/assets/js/main.js
+++ ass2 bootstrap/assets/js/main.js
@@ -4,6 +4,10 @@
 //Validation for searchTransportForm
 function validateForm() {
 
+    var theForm = document.querySelector("#searchTransportForm");
+
+    theForm.noValidate = true;
+
     var errors = [];
 
     if (document.getElementById("pickUpLocation").value === "Select pick up location") {
@@ -79,29 +83,51 @@ document.getElementById("cardSelectButton").onclick = function () {
 // }
 
 
-//Validation for confirmationForm  - Check that characters only letters
+//Validation for confirmationForm 
 function validateConfirmationForm() {
+
+    var theForm = document.querySelector("#confirmationForm"); 
+    theForm.noValidate = true;//to remove html5 validation - do I really need it?
+
     var errors = [];
 
     var firstName = document.getElementById("inputFirstName").value.trim();
     var lastName = document.getElementById("inputLastName").value.trim();
+    var phoneNumber = document.getElementById("inputPhone").value.trim();
+    var emailAddress = document.getElementById("inputEmail").value.trim();
 
     if (firstName === "") {
         errors.push("- First name is required");
-    } else if (!/^[A-Za-z]+$/.test(firstName)) {//Check that characters only letters
+    } else if (!/^[A-Za-z]+$/.test(firstName)) {//Check characters - only letters allowed
         errors.push("- First name should contain only letters.");
     }
 
     if (lastName === "") {
         errors.push("- Last name is required");
-    } else if (!/^[A-Za-z]+$/.test(lastName)) {
+    } else if (!/^[A-Za-z]+$/.test(lastName)) {//Check characters - only letters allowed
         errors.push("- Last name should contain only letters.");
+    }
+
+    if (phoneNumber === "") {
+        errors.push("- Phone number is required");
+    } else if (/^[A-Za-z]+$/.test(phoneNumber)) {//Check characters - only digits allowed 
+        errors.push("- Phone number should contain only digits.");
+    
+    } else if (!/^\+[\d\s\-()]+$/.test(phoneNumber)) {//Check format - "+" in the begining followed by numbers /^\d+$/  ^\\+[1-9]\\\\d{1,14}$
+        errors.push("- Phone number should be in the format +6402041975383");
+    }
+
+    if (emailAddress === "") {
+        errors.push("- Email address is required.");
+        
+    } else if (!/^([a-z0-9_.\-+]+)@([\da-z.-]+)(\.([a-z]{2,}))+$/.test(emailAddress)) {//Validate email format
+        errors.push("- Invalid email address.");
     }
 
     if (errors.length > 0) {
         errors.unshift("Please fill in all required fields correctly:");
         alert(errors.join("\n"));
-        return false;
+        return false;  
     }
 
     document.getElementById("confirmationForm").classList.add("hidden");
